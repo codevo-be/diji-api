b47b2029d178e4bfdaad220084246abc438dfc68
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-04 10:57:44 +0200

    Feat : Update task management API to include task columns resource

--- modules/task/Routes/api.php
+++ modules/task/Routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use Diji\Task\Http\Controllers\ColumnController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix'     => 'api',
 ], function () {
     Route::middleware(['auth:api', "auth.tenant"])->group(function(){
-        Route::resource("/tasks", TaskController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
+        Route::resource("/task-columns", ColumnController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
     });
 });
 
