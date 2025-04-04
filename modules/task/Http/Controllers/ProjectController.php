63bfa590ef98e4510621a3f6254bdea957904923
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-04 12:36:04 +0200

    Feat : Simplify index method in ProjectController by removing unnecessary Request parameter

--- modules/task/Http/Controllers/ProjectController.php
+++ modules/task/Http/Controllers/ProjectController.php
@@ -5,11 +5,10 @@ namespace Diji\Task\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Diji\Task\Models\Project;
 use Diji\Task\Resources\ProjectResource;
-use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $query = Project::query();
 
