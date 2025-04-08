bbe765ec68735203020def1ffcdda823831bd77a
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-08 13:14:59 +0200

    Feat : Add bulk update method to ItemController and corresponding route for batch task updates

--- modules/task/Http/Controllers/ItemController.php
+++ modules/task/Http/Controllers/ItemController.php
@@ -8,6 +8,7 @@ use Diji\Task\Http\Requests\UpdateItemRequest;
 use Diji\Task\Models\Item;
 use Diji\Task\Resources\ItemResource;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
@@ -33,5 +34,32 @@ class ItemController extends Controller
         return response()->json([
             'data' => new ItemResource($item),
         ]);
+    }
+
+    public function bulkUpdate(Request $request)
+    {
+        $tasks = $request->input('tasks');
+
+        if (!is_array($tasks)) {
+            return response()->json(['error' => 'Le format des données est invalide.'], 400);
+        }
+
+        foreach ($tasks as $taskData) {
+            if (!isset($taskData['id'])) {
+                continue;
+            }
+
+            $task = Item::find($taskData['id']);
+
+            if ($task) {
+                $validFields = array_intersect_key($taskData, array_flip([
+                    'name', 'description', 'order', 'task_column_id', 'status', 'priority'
+                ]));
+
+                $task->update($validFields);
+            }
+        }
+
+        return response()->json(['message' => 'Mise à jour des tâches réussie.']);
     }
 }
