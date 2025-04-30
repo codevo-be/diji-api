62efc101e363efa7f81dde9cde55f3d8192206c4
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-30 09:55:38 +0200

    Refactor : Remove TaskServiceProvider from services configuration

--- bootstrap/cache/services.php
+++ bootstrap/cache/services.php
@@ -39,7 +39,6 @@
     35 => 'Barryvdh\\DomPDF\\ServiceProvider',
     36 => 'Diji\\Billing\\BillingServiceProvider',
     37 => 'Diji\\Contact\\ContactServiceProvider',
-    38 => 'Diji\\Task\\TaskServiceProvider',
   ),
   'eager' => 
   array (
@@ -66,7 +65,6 @@
     20 => 'Barryvdh\\DomPDF\\ServiceProvider',
     21 => 'Diji\\Billing\\BillingServiceProvider',
     22 => 'Diji\\Contact\\ContactServiceProvider',
-    23 => 'Diji\\Task\\TaskServiceProvider',
   ),
   'deferred' => 
   array (
