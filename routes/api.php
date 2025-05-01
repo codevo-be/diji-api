6bbb6eba5a3e6bb41de82795752ab45aacffbba5
Maxime Fondu <maximefondu@icloud.com>
2025-05-01 11:03:15 +0200

    add route reset password

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post("/auth/forgot-password", [\App\Http\Controllers\AuthController::class, 'forgotPassword']);
+Route::post("/auth/reset-password", [\App\Http\Controllers\AuthController::class, 'resetPassword']);
 
 
 Route::middleware(['auth:api','auth.tenant'])->prefix('auth')->group(function () {
