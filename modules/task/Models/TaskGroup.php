6a7e156543876469f9dda1eb954064ba7101c870
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-05-09 16:29:34 +0200

    Feat : Implement position management and task_number assignment for TaskGroup and TaskItem models

--- modules/task/Models/TaskGroup.php
+++ modules/task/Models/TaskGroup.php
@@ -19,6 +19,35 @@ class TaskGroup extends Model
         'project_id'
     ];
 
+    protected static function booted()
+    {
+        parent::boot();
+
+        static::creating(function (TaskGroup $group) {
+            if(!$group->position){
+                $group->position = TaskGroup::where('project_id', $group->project_id)->count() + 1;
+            }
+        });
+
+        static::updating(function (TaskGroup $group) {
+            $postion_before = $group->getOriginal('position');
+            $postion_current = $group->position;
+
+            if ($postion_before !== $postion_current) {
+                if ($postion_before < $postion_current) {
+                    TaskGroup::where('project_id', $group->project_id)
+                        ->whereBetween('position', [$postion_before + 1, $postion_current])
+                        ->decrement('position');
+                } else {
+                    TaskGroup::where('project_id', $group->project_id)
+                        ->whereBetween('position', [$postion_current, $postion_before - 1])
+                        ->increment('position');
+                }
+            }
+        });
+    }
+
+
     public function items(): HasMany
     {
         return $this->hasMany(TaskItem::class, 'task_group_id')->orderBy('position');
--- modules/task/Models/TaskItem.php
+++ modules/task/Models/TaskItem.php
@@ -36,6 +36,42 @@ class TaskItem extends Model
         'position',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (TaskItem $item) {
+            if(!$item->position){
+                $item->position = TaskItem::where('task_group_id', $item->task_group_id)->count() + 1;
+            }
+
+            $group = TaskGroup::findOrFail($item->task_group_id);
+
+            $last_task_number = self::whereHas('group', function ($q) use($group) {
+                $q->where('project_id', $group->project_id);
+            })->max('task_number');
+
+            $item->task_number = $last_task_number ? $last_task_number + 1 : 1;
+        });
+
+        static::updating(function (TaskItem $item) {
+            $postion_before = $item->getOriginal('position');
+            $postion_current = $item->position;
+
+            if ($postion_before !== $postion_current) {
+                if ($postion_before < $postion_current) {
+                    TaskItem::where('task_group_id', $item->task_group_id)
+                        ->whereBetween('position', [$postion_before + 1, $postion_current])
+                        ->decrement('position');
+                } else {
+                    TaskItem::where('task_group_id', $item->task_group_id)
+                        ->whereBetween('position', [$postion_current, $postion_before - 1])
+                        ->increment('position');
+                }
+            }
+        });
+    }
+
     public function group()
     {
         return $this->belongsTo(TaskGroup::class, 'task_group_id');
