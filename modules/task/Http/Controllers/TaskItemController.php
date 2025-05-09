30d69f47ab7befb0c85f83595cf1aac7cba65f09
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-05-09 16:24:46 +0200

    Feat : Implement bulk update for task items with validation

--- modules/task/Http/Controllers/TaskItemController.php
+++ modules/task/Http/Controllers/TaskItemController.php
@@ -3,11 +3,13 @@
 namespace Diji\Task\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Diji\Task\Http\Requests\BulkUpdateTaskItemRequest;
 use Diji\Task\Http\Requests\StoreTaskItemRequest;
 use Diji\Task\Http\Requests\UpdateTaskItemRequest;
 use Diji\Task\Models\TaskItem;
 use Diji\Task\Resources\TaskItemResource;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class TaskItemController extends Controller
@@ -43,5 +45,27 @@ class TaskItemController extends Controller
         $item->delete();
 
         return response()->noContent();
+    }
+
+    public function bulkUpdate(BulkUpdateTaskItemRequest $request, int $project): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $updated = [];
+
+        foreach ($validated['tasks'] as $taskData) {
+            $task = TaskItem::find($taskData['id']);
+            if ($task) {
+                $task->update([
+                    'position' => $taskData['position'],
+                    'task_group_id' => $taskData['task_group_id'],
+                ]);
+                $updated[] = new TaskItemResource($task);
+            }
+        }
+
+        return response()->json([
+            'data' => $updated,
+        ]);
     }
 }
